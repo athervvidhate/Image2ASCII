0f86f80f972fcb144268d36b2f3365a6e9c99c68
atherv <athervvidhate@gmail.com>
2024-07-08 23:00:47 -0700

    edited readme, added comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Image2ASCII
+
+This is a simple Python script that opens an image that you input and outputs it as ASCII art. It only takes .jpg, .jpeg, and .png. An example image of a monkey's selfie has been provided!
+
+## Directions
+Download the file, then copy your image into the same directory as the script. Then open Terminal/Command Prompt and run `python3 image2ascii.py <FILENAME>`.
+> ex: `python3 image2ascii.py <Capture-2024-07-08-224516.png>`
+The resulting image will then be outputted for you to copy and paste!
+
+In the script, there is a variable `targetWidth`. This has been set to a default value of `80`, but can be changed to change how the ASCII art looks.
--- image2ascii.py
+++ image2ascii.py
@@ -4,20 +4,33 @@ import sys
 import numpy as np
 
 def convert():
+
     filename = sys.argv[1]
     asciiRep = ""
-    targetWidth = 80
+
+    # Default width of 80 for command line interfaces, can be edited to change how the image looks.
+    targetWidth = 80 
+
+    # Resizes the image to be smaller, so that the function has less data to go through
     with Image.open(filename) as img:
         aspectRatio = img.height / img.width
         targetHeight = int(aspectRatio*targetWidth*0.55)
         img = img.resize((targetWidth, targetHeight))
     
+    # Contains all the different brightness levels for each pixel
     asciiMap = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', '&', '8', 'B', 'W', '#', '%']
+
+    # Creates a list of all of the RGB values for each pixel in the image
     pixels = list(img.getdata())
+
+    # Creates the ASCII art
     for i in range(len(pixels)):
         if (i % img.width == 0):
             asciiRep += "\n"
+        
+        # Takes the average of the RGB values to get a brightness value
         avg = np.mean(pixels[i])
+
         asciiRep += asciiMap[int(avg) * len(asciiMap) // 256]
     print(asciiRep)
 
